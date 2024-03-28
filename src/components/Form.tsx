3e5b7ca3e8d68eca0afb9f373eee4ca5e5c70306
gabicilfone <cilfonegabriel@gmail.com>
2024-03-28 16:33:09 -0300

    Creatinf the form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,8 +1,18 @@
 
-export default function form() {
+export default function Form() {
   return (
-    <div>
-      Form
-    </div>
+    <form
+        className="space-y-5 bg-white shadow p-10 rounded-lg"
+    >
+        <div className="grid grid-cols-1 gap-3">
+            <label htmlFor="category">Categorías</label>
+            <select
+                className="border border-slate-300 p-2 rounded-lg w-full bg-white"
+                id="category"
+            >   
+
+            </select>
+        </div>
+    </form>
   )
 }
