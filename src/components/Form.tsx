fab1b2f776192be95358965504af7dfb45188658
gabicilfone <cilfonegabriel@gmail.com>
2024-04-01 12:32:35 -0300

    Add form validation and enable button

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -21,6 +21,12 @@ export default function Form() {
         })
     }
 
+    const isValidActivity = () => {
+        const {name, calories} = activity
+
+        return name.trim() !== '' && calories > 0
+    }
+
   return (
     <form
         className="space-y-5 bg-white shadow p-10 rounded-lg"
@@ -70,8 +76,9 @@ export default function Form() {
 
         <input
             type="submit"
-            className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer"
+            className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer disabled:opacity-10"
             value="Guardar Comida o Guardar Ejercicio"
+            disabled={!isValidActivity()}
         />
 
     </form>
