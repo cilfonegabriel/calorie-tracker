3034880cf15351a61660c23e2b1ec38b844ab911
gabicilfone <cilfonegabriel@gmail.com>
2024-04-08 09:36:54 -0300

    Restar activeId

--- src/reducers/activity-reducer.ts
+++ src/reducers/activity-reducer.ts
@@ -30,7 +30,8 @@ export const activityReducer = (
 
       return {
         ...state,
-        activities : updatedActivities
+        activities : updatedActivities,
+        activeId: ''
       }
     }
 
