08f340f498ca3320d41f44891fff935097b34709
gabicilfone <cilfonegabriel@gmail.com>
2024-04-09 09:55:21 -0300

    Show calories burned

--- src/components/CalorieTracker.tsx
+++ src/components/CalorieTracker.tsx
@@ -8,6 +8,8 @@ type CalorieTrackerProps = {
 export default function CalorieTracker({activities}: CalorieTrackerProps) {
   
     const caloriesConsumed = useMemo(() => activities.reduce((total, activity) => activity.category === 1 ? total + activity.calories : total, 0), [activities])
+    const caloriesBurned = useMemo(() => activities.reduce((total, activity) => activity.category === 2 ? total + activity.calories : total, 0), [activities])
+
     return (
     <>
         <h2 className="text-4xl font-black text-white text-center">Resumen de Calorias</h2>
@@ -17,6 +19,13 @@ export default function CalorieTracker({activities}: CalorieTrackerProps) {
                 {caloriesConsumed}
                 </span>
                 Consumidas
+            </p>
+
+            <p className="text-white font-bold rounded-full grid grid-cols-1 gap-3 text-center">
+                <span className="font-black text-6xl text-orange">
+                {caloriesBurned}
+                </span>
+                Ejercicio
             </p>
         </div>
     </>
